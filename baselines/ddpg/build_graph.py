5596e797d25eceb53d99b906df51d566ad61a395
ngc92 <ngc92@users.noreply.github.com>
2017-06-05 19:03:18 +0200

    started ddpg

---
+++ baselines/ddpg/build_graph.py
@@ -0,0 +1,201 @@
+"""Deep Deterministic Policy Gradient learning graph
+
+The functions in this file are used to create the following functions:
+
+======= act ========
+
+    Function to chose an action given an observation
+
+    Parameters
+    ----------
+    observation: object
+        Observation that can be feed into the output of make_obs_ph
+    stochastic: bool
+        if set to False all the actions are always deterministic (default False)
+    update_eps_ph: float
+        update epsilon a new value, if negative not update happens
+        (default: no update)
+
+    Returns
+    -------
+    Tensor of dtype tf.float32 and shape (BATCH_SIZE, NUM_ACTIONS) with an action to be performed for
+    every element of the batch.
+"""
+import tensorflow as tf
+import baselines.common.tf_util as U
+
+
+def build_act(make_obs_ph, policy, noise, num_actions, scope="deepq", reuse=None):
+    """Creates the act function:
+
+    Parameters
+    ----------
+    make_obs_ph: str -> tf.placeholder or TfInput
+        a function that take a name and creates a placeholder of input with that name
+    policy: (tf.Tensor, int, str, bool) -> tf.Tensor
+        the model that takes the following inputs:
+            observation_in: object
+                the output of observation placeholder
+            num_actions: int
+                number of actions
+            scope: str
+            reuse: bool
+                should be passed to outer variable scope
+        and returns a tensor of shape (batch_size, num_actions) with values of every action.
+    noise: (int, str, bool) -> tf.Tensor
+        takes the following inputs
+            num_actions: int
+            scope: str
+            reuse: bool
+        and returns a tensor (batch_size, num_actions) of noise.
+    num_actions: int
+        number of actions.
+    scope: str or VariableScope
+        optional scope for variable_scope.
+    reuse: bool or None
+        whether or not the variables should be reused. To be able to reuse the scope must be given.
+
+    Returns
+    -------
+    act: (tf.Variable, bool, float) -> tf.Variable
+        function to select and action given observation.
+`       See the top of the file for details.
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        observations_ph = U.ensure_tf_input(make_obs_ph("observation"))
+        stochastic_ph = tf.placeholder(tf.bool, (), name="stochastic")
+        update_eps_ph = tf.placeholder(tf.float32, (), name="update_eps")
+
+        eps = tf.get_variable("eps", (), initializer=tf.constant_initializer(0))
+
+        deterministic_actions = policy(observations_ph.get(), num_actions, scope="policy")
+        action_noise = noise(num_actions, scope="noise")
+        stochastic_actions = deterministic_actions + eps * action_noise
+
+        output_actions = tf.cond(stochastic_ph, lambda: stochastic_actions, lambda: deterministic_actions)
+        update_eps_expr = eps.assign(tf.cond(update_eps_ph >= 0, lambda: update_eps_ph, lambda: eps))
+
+        act = U.function(inputs=[observations_ph, stochastic_ph, update_eps_ph],
+                         outputs=output_actions,
+                         givens={update_eps_ph: -1.0, stochastic_ph: True},
+                         updates=[update_eps_expr])
+        return act
+
+def build_train(make_obs_ph, policy, noise, critic, num_actions, optimizer, grad_norm_clipping=None, gamma=1.0, scope="ddpg", reuse=None):
+    """Creates the act function:
+
+    Parameters
+    ----------
+    make_obs_ph: str -> tf.placeholder or TfInput
+        a function that take a name and creates a placeholder of input with that name
+    policy: (tf.Variable, int, str, bool) -> tf.Variable
+        the model that takes the following inputs:
+            observation_in: object
+                the output of observation placeholder
+            num_actions: int
+                number of actions
+            scope: str
+            reuse: bool
+                should be passed to outer variable scope
+        and returns a tensor of shape (batch_size, num_actions) with values of every action.
+    num_actions: int
+        number of actions
+    reuse: bool
+        whether or not to reuse the graph variables
+    optimizer: tf.train.Optimizer
+        optimizer to use for the Q-learning objective.
+    grad_norm_clipping: float or None
+        clip graident norms to this value. If None no clipping is performed.
+    gamma: float
+        discount rate.
+    scope: str or VariableScope
+        optional scope for variable_scope.
+    reuse: bool or None
+        whether or not the variables should be reused. To be able to reuse the scope must be given.
+
+    Returns
+    -------
+    act: (tf.Variable, bool, float) -> tf.Variable
+        function to select and action given observation.
+`       See the top of the file for details.
+    train: (object, np.array, np.array, object, np.array, np.array) -> np.array
+        optimize the error in Bellman's equation.
+`       See the top of the file for details.
+    update_target: () -> ()
+        copy the parameters from optimized Q function to the target Q function.
+`       See the top of the file for details.
+    debug: {str: function}
+        a bunch of functions to print debug data like q_values.
+    """
+    act_f = build_act(make_obs_ph, policy, noise, num_actions, scope=scope, reuse=reuse)
+
+    with tf.variable_scope(scope, reuse=reuse):
+        # set up placeholders
+        obs_t_input = U.ensure_tf_input(make_obs_ph("obs_t"))
+        act_t_ph = tf.placeholder(tf.float32, [None, num_actions], name="action")
+        rew_t_ph = tf.placeholder(tf.float32, [None], name="reward")
+        obs_tp1_input = U.ensure_tf_input(make_obs_ph("obs_tp1"))
+        done_mask_ph = tf.placeholder(tf.float32, [None], name="done")
+        importance_weights_ph = tf.placeholder(tf.float32, [None], name="weight")
+
+        # q network evaluation
+        q_t = critic(obs_t_input.get(), act_t_ph, scope="critic")
+        q_func_vars = U.scope_vars(U.absolute_scope_name("critic"))
+
+        # target q network evalution
+        q_tp1 = q_func(obs_tp1_input.get(), num_actions, scope="target_q_func")
+        target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
+
+        # q scores for actions which we know were selected in the given state.
+        q_t_selected = tf.reduce_sum(q_t * tf.one_hot(act_t_ph, num_actions), 1)
+
+        # compute estimate of best possible value starting from state at t + 1
+        if double_q:
+            q_tp1_using_online_net = q_func(obs_tp1_input.get(), num_actions, scope="q_func", reuse=True)
+            q_tp1_best_using_online_net = tf.arg_max(q_tp1_using_online_net, 1)
+            q_tp1_best = tf.reduce_sum(q_tp1 * tf.one_hot(q_tp1_best_using_online_net, num_actions), 1)
+        else:
+            q_tp1_best = tf.reduce_max(q_tp1, 1)
+        q_tp1_best_masked = (1.0 - done_mask_ph) * q_tp1_best
+
+        # compute RHS of bellman equation
+        q_t_selected_target = rew_t_ph + gamma * q_tp1_best_masked
+
+        # compute the error (potentially clipped)
+        td_error = q_t_selected - tf.stop_gradient(q_t_selected_target)
+        errors = U.huber_loss(td_error)
+        weighted_error = tf.reduce_mean(importance_weights_ph * errors)
+        # compute optimization op (potentially with gradient clipping)
+        if grad_norm_clipping is not None:
+            optimize_expr = U.minimize_and_clip(optimizer,
+                                                weighted_error,
+                                                var_list=q_func_vars,
+                                                clip_val=grad_norm_clipping)
+        else:
+            optimize_expr = optimizer.minimize(weighted_error, var_list=q_func_vars)
+
+        # update_target_fn will be called periodically to copy Q network to target Q network
+        update_target_expr = []
+        for var, var_target in zip(sorted(q_func_vars, key=lambda v: v.name),
+                                   sorted(target_q_func_vars, key=lambda v: v.name)):
+            update_target_expr.append(var_target.assign(var))
+        update_target_expr = tf.group(*update_target_expr)
+
+        # Create callable functions
+        train = U.function(
+            inputs=[
+                obs_t_input,
+                act_t_ph,
+                rew_t_ph,
+                obs_tp1_input,
+                done_mask_ph,
+                importance_weights_ph
+            ],
+            outputs=td_error,
+            updates=[optimize_expr]
+        )
+        update_target = U.function([], [], updates=[update_target_expr])
+
+        q_values = U.function([obs_t_input], q_t)
+
+        return act_f, train, update_target, {'q_values': q_values}
